a73a59c34ad5a68a36fab02eebf4e9d11db4327e
hoanito <hhoang219@gmail.com>
2022-10-06 11:30:39 +0700

    Add docker helper

--- staging/docker_helper.sh
+++ staging/docker_helper.sh
@@ -1,3 +1,10 @@
-docker ps | grep massbit | cut -d "/" -f 2 | cut -d " " -f 1
+_get_running_tag ()
+{
+	docker ps | grep massbit | cut -d "/" -f 2 | cut -d " " -f 1
+}
 
-docker rm  $(docker ps | grep _10 | cut -d " " -f 1) -f
+_cleanup ()
+{
+	docker rm  $(docker ps | grep _10 | cut -d " " -f 1) -f
+}
+$@
